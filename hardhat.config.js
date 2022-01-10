29912f9c3f61cf456348bf2802bad988de30f663
jango <77952627+mejango@users.noreply.github.com>
2022-01-10 17:56:26 -0300

    v1.1 deployed to mainnet. version bumped. (#14)

--- hardhat.config.js
+++ hardhat.config.js
@@ -40,7 +40,7 @@ module.exports = {
     },
     mainnet: {
       url: 'https://mainnet.infura.io/v3/' + infuraId,
-      gasPrice: 50000000000,
+      gasPrice: 160000000000,
       accounts: {
         mnemonic: mnemonic(),
       },
