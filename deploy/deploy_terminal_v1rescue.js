aca97664d0c4e9bf301272452bd0767dc2bba6da
mejango <mejango@protonmail.com>
2022-02-15 11:17:43 -0500

    added rescue terminal deploy

---
+++ deploy/deploy_terminal_v1rescue.js
@@ -0,0 +1,53 @@
+/**
+ * Deploys Terminal V1rescue contract.
+ */
+
+module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  let chainId = await getChainId();
+
+  let chain;
+  let multisig;
+
+  console.log({ chainId, d: deployer });
+
+  switch (chainId) {
+    // mainnet
+    case '1':
+      chain = 'mainnet';
+      multisig = '0xAF28bcB48C40dBC86f52D459A6562F658fc94B1e';
+      break;
+    // rinkeby
+    case '4':
+      chain = 'rinkeby';
+      multisig = '0xAF28bcB48C40dBC86f52D459A6562F658fc94B1e';
+      break;
+    // local
+    case '31337':
+      chain = 'localhost';
+      multisig = '0x69C6026e3938adE9e1ddE8Ff6A37eC96595bF1e1';
+      break;
+    default:
+      throw new Error(`Chain id ${chainId} not supported`);
+  }
+
+  console.log({ chain });
+  console.log({ owner: multisig });
+
+  await deploy('TerminalV1Rescue', {
+    from: deployer,
+    args: [
+      require(`../deployments/${chain}/Projects.json`).address,
+      require(`../deployments/${chain}/FundingCycles.json`).address,
+      require(`../deployments/${chain}/TicketBooth.json`).address,
+      require(`../deployments/${chain}/OperatorStore.json`).address,
+      require(`../deployments/${chain}/TerminalDirectory.json`).address,
+      /*_governance=*/multisig
+    ],
+    log: true,
+  });
+};
+
+module.exports.tags = ['TerminalV1Rescue'];
